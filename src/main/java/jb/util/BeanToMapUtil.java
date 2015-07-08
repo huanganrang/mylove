fe1ef03a1202be3da6b4ef83ad32fa5a36346f39
xuwenming <252429711@qq.com>
2015-07-08 17:59:25 +0800

    对象-MAP之间转换工具类

---
+++ src/main/java/jb/util/BeanToMapUtil.java
@@ -0,0 +1,62 @@
+package jb.util;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeanToMapUtil {
+	
+	/**
+	 * @param o
+	 * @param cProperties
+	 * @param ucProperties
+	 * @return
+	 * @throws IntrospectionException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Map convertBean(Object o, String[] cProperties, String[] ucProperties, boolean isNull) throws IntrospectionException, IllegalArgumentException, IllegalAccessException, InvocationTargetException{
+        Class type = o.getClass();
+        Map returnMap = new HashMap();
+        BeanInfo beanInfo = Introspector.getBeanInfo(type);
+
+        PropertyDescriptor[] propertyDescriptors =  beanInfo.getPropertyDescriptors();
+        for (int i = 0; i< propertyDescriptors.length; i++) {
+            PropertyDescriptor descriptor = propertyDescriptors[i];
+            String propertyName = descriptor.getName();
+            if (!propertyName.equals("class")) {
+                Method readMethod = descriptor.getReadMethod();
+                Object result = readMethod.invoke(o, new Object[0]);
+                if (result != null) {
+                    returnMap.put(propertyName, result);
+                } else {
+                    returnMap.put(propertyName, "");
+                }
+            }
+        }
+        return returnMap;
+    }
+	
+	@SuppressWarnings("rawtypes")
+	public static Map convertBean(Object o, String[] cProperties, boolean isNull) throws IllegalArgumentException, IntrospectionException, IllegalAccessException, InvocationTargetException{
+		return convertBean(o, cProperties, null, isNull);
+    }
+	
+	@SuppressWarnings("rawtypes")
+	public static Map convertBean(Object o) throws IllegalArgumentException, IntrospectionException, IllegalAccessException, InvocationTargetException{
+		return convertBean(o, null, null, true);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static Map convertBean(Object o, boolean isNull) throws IllegalArgumentException, IntrospectionException, IllegalAccessException, InvocationTargetException{
+		return convertBean(o, null, null, isNull);
+	}
+	
+}
