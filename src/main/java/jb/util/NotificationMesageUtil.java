a2b024b36a6618df77ea7b703d24bbc33d092145
huangzhi <huangzhi@huangzhi-PC>
2015-07-29 23:00:39 +0800

    徐文明是个大坏蛋

--- src/main/java/jb/util/NotificationMesageUtil.java
+++ src/main/java/jb/util/NotificationMesageUtil.java
@@ -1,16 +1,31 @@
 package jb.util;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import jb.android.push.NotificationManager;
 
 import org.androidpn.server.xmpp.XmppServer;
+import org.androidpn.server.xmpp.session.ClientSession;
+import org.androidpn.server.xmpp.session.SessionManager;
 
 public class NotificationMesageUtil {
 	public static boolean notifMessage(String openId,String message){
 		try{
 			NotificationManager notif = (NotificationManager) XmppServer
 					.getInstance().getBean("notificationManager");
-			notif.sendNotifcationToUser("1234567890", openId, "title", message,
-					"uri");
+			Collection<ClientSession> sessions = SessionManager.getInstance()
+					.getSessions();
+			Set<ClientSession> usernames = new HashSet<ClientSession>();
+			for (ClientSession cs : sessions) {
+					if(cs.getUsername().equals(openId))
+					usernames.add(cs);
+
+			}
+			notif.sendNotifcationToSession("1234567890", "test", "title", message,
+					"uri",
+					usernames.toArray(new ClientSession[usernames.size()]));
 		}catch(Exception e){
 			e.printStackTrace();
 			return false;
